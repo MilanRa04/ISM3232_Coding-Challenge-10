6f563520134962768391ccec49e414709e2501b4
Milan Ranpariya <ranpariya04@gmail.com>
2024-10-20 22:55:57 -0400

    Create checkout event and confirmation message

--- main.js
+++ main.js
@@ -23,3 +23,16 @@ sizeSelect.addEventListener('change', function () {
 
 // Task 3: Handle Stock Availability
 // The code for task 3 is added into task 2 at lines 6, 13, and 17-21.
+
+
+//Task 4: Create a Checkout Event
+purchaseButton.addEventListener('click', function () {
+    const selectedOption = sizeSelect.options[sizeSelect.selectedIndex];
+    const stock = selectedOption.getAttribute('data-stock');
+
+    if (stock > 0) {
+        alert('Product added to cart successfully!');
+    } else {
+        alert('Sorry, this product is out of stock.');
+    }
+});
